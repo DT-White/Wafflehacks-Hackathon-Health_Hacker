166ca02c0de232117857549c4098cb6353158dfd
Andrew Wahba <awahba0426@gmail.com>
2022-06-17 17:56:58 -0400

    FIRST UPDATE BABYgit add -Agit add -A

---
+++ application.py
@@ -0,0 +1,22 @@
+import pygame;
+
+def main_menu():
+    pass
+
+def mood_screen():
+    pass
+
+def social_energy_freetime_screen():
+    pass
+
+def exercise_diet_sleep_screen():
+    pass
+
+def journal_screen():
+    pass
+
+def analysis_screen():
+    pass
+
+def quit_screen():
+    pass
