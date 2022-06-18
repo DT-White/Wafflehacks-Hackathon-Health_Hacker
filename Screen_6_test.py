8e4976b6f0430dce114f266264756f1d0c45f180
Lap <big_lappie@yahoo.com>
2022-06-18 18:54:20 -0400

    jentry 4 screen

--- Screen_6_test.py
+++ Screen_6_test.py
@@ -7,6 +7,31 @@ SCREEN_WIDTH = 864
 WIN = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("practice screen")
 print(WIN.get_size())
+WHITE = (255,255,255)
+FPS = 60
+
+background_image = pygame.image.load(os.path.join('resource', 'tab_journal_selected.png')) #load an image as a surface
+button_image = pygame.image.load(os.path.join('resource', 'button_quit.png')).convert_alpha() #load an image, convert alpha preserves transparency
+button_xy = (500,500)
+button_rect = button_image.get_rect(topleft = button_xy)
+
+def draw_image(image, xy):
+    WIN.blit(image, xy) #Screen.blit(image, (x,y))
+
+
+def draw_bg(color):
+    WIN.fill(color) #fill the window with color, color is a tuple variable (R,G,B)
+
+
+def draw_rect():
+    #pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.rect(WIN, (200,200,200), pygame.Rect(100,100,150,150))
+    
+
+def draw_line():
+    #pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.line(WIN, (0,0,0), (0,0), (600,100), 6)
+    
 
 
 def main():
@@ -24,18 +49,19 @@ def main():
                 if(button_rect.collidepoint(event.pos)): #and the position of the click collides with the x_y for the button
                     pygame.quit() #quit
                     exit() #terminate
-
+            if event.type == pygame.MOUSEBUTTONDOWN: #If the user clicked
+                print(event.pos)
        
         draw_bg(WHITE) 
         draw_image(background_image, (0,0))
         draw_image(button_image, button_xy)
-      #  draw_rect()
-      #  draw_line()
+        draw_rect()
+        draw_line()
         
         pygame.display.flip()#This updates the screen to show all changes     
         
     pygame.quit()
-    
+
 #__name__ prevents python from running file whenever it is imported by other files
 if __name__ == "__main__":
     main()
